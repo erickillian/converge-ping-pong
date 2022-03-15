0271310d272001d92be88efd0be513949ab089c7
Eric Killian <erickillian@protonmail.com>
2022-03-14 23:34:45 -0400

    Debug = False

--- pongboard/settings.py
+++ pongboard/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', False))
 
 DEFAULT_ALLOWED_HOSTS = '127.0.0.1 localhost 0.0.0.0'
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '0.0.0.0', 'converge-ping-pong.herokuapp.com']# ['converge-ping-pong.herokuapp.com', 'localhost']
